357544186a00e6cb1613fa7406a459fdb4a6afb8
Raphael Pontes Campos <furacker@gmail.com>
2022-12-05 23:13:05 +0100

    Added GoRouter instead of Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'dart:async';
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   // * For more info on error handling, see:
   // * https://docs.flutter.dev/testing/errors
   await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    usePathUrlStrategy();
     // * Entry point of the app
     runApp(const MyApp());
 
